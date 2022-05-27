ddb9fd36f7cfa026f382bcde9bcf4098f2a5bc6c
BajcarM <102542768+BajcarM@users.noreply.github.com>
2022-05-27 21:29:01 +0200

    dodělán JS a bonusové zadání

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,9 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector("nav");
 const body = document.querySelector("body");
 const menuItems = document.querySelectorAll(".menu-nav__item");
+const articles = document.querySelectorAll("article");
+const headers = document.querySelectorAll("h2");
+const socialIcons = document.querySelector(".social");
 
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open");
@@ -21,3 +24,37 @@ document.addEventListener("keydown", (e) => {
     menu.classList.remove("open");
   }
 });
+
+let averageHeight = 0;
+articles.forEach((article) => {
+  averageHeight += article.getBoundingClientRect().height / articles.length;
+});
+
+let sumOfHeaders = 0;
+headers.forEach((header) => {
+  sumOfHeaders += header.innerText.split(" ").length;
+});
+
+articles.forEach((article) => {
+  article.addEventListener("click", (e) => {
+    console.log({
+      name: e.currentTarget.querySelector("h2").innerText,
+      date: e.currentTarget.querySelector("span").innerText.split("/")[0],
+      length: e.currentTarget.querySelector("span").innerText.split("/")[1],
+    });
+  });
+});
+
+window.addEventListener("scroll", () => {
+  console.log(
+    `viewport to top  ${
+      socialIcons.getBoundingClientRect().top
+    }, document to top ${
+      window.scrollY + socialIcons.getBoundingClientRect().top
+    }`
+  );
+});
+
+console.log(`Number of articles - including banner: ${articles.length}`);
+console.log(`Average height of articles - including banner: ${averageHeight}`);
+console.log(`Average height of articles - including banner: ${sumOfHeaders}`);
